7965c45904eea2b4b32d6eb416ebabb9446b6761
Harlen Lopez <140995603+harlenlopez10@users.noreply.github.com>
2025-11-08 16:03:35 -0600

    Estructura + FS

---
+++ ProyectoVotoSeguro/ProyectoVotoSeguro/Services/FirebaseServices.cs
@@ -0,0 +1,45 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Auth;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Firestore;
+
+namespace ProyectoVotoSeguro.Services;
+
+public class FirebaseServices
+{
+    private readonly FirestoreDb _firestoreDb;
+    private readonly string _projectId;
+
+    
+    
+    public FirebaseServices(IConfiguration configuration)
+    {
+        _projectId = configuration["Firebase:ProjectId"]
+                     ?? throw new InvalidOperationException("Firebase ProjectID no configurado");
+
+//Inicializar Firebase App si no esta inicializado
+        if (FirebaseApp.DefaultInstance == null)
+        {
+            var credential = GoogleCredential.GetApplicationDefault();
+            FirebaseApp.Create(new AppOptions
+            {
+                Credential = credential,
+                ProjectId = _projectId
+            });
+
+        }
+
+//Crear instancia de Firebase
+        _firestoreDb = FirestoreDb.Create(_projectId); 
+    }
+
+    public FirestoreDb GetFirestoreDb()
+    {
+        return _firestoreDb;
+    }
+
+    public CollectionReference GetCollection(string collectionName)
+    {
+        return _firestoreDb.Collection(collectionName); 
+    }
+}
